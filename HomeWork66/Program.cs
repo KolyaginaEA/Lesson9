f773581149ad770fb46b00366216d2eb5b6b6afa
Ekaterina <katya_usova@bk.ru>
2023-02-11 22:54:18 +0300

    HomeWork66

---
+++ HomeWork66/Program.cs
@@ -0,0 +1,19 @@
+﻿System.Console.Write("M = ");
+int M = System.Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("(N > M) N = ");
+int N = System.Convert.ToInt32(System.Console.ReadLine());
+
+int sum = 0;
+System.Console.Write("Сумма: {0}", Sum(N));
+System.Console.ReadKey();
+
+int Sum(int n)
+{
+    if (n != M - 1)
+    {
+        sum += n;
+        n--;
+        Sum(n);
+    }
+    return sum;
+}
