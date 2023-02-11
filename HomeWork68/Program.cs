9eb9dff206303b765652fe9d20bb19010d326cab
Ekaterina <katya_usova@bk.ru>
2023-02-11 22:57:23 +0300

    HomeWork68

---
+++ HomeWork68/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write("M = ");
+int M = System.Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("N = ");
+int N = System.Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.Write("A({0}, {1}) = {2}", M, N, A(M, N));
+System.Console.ReadKey();
+
+int A(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) return A(m - 1, 1);
+    if (m > 0 && m > 0) return A(m - 1, A(m, n - 1));
+    return A(m, n);
+}
