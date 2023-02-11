9438b46ea8288569710b60651c52d214d2f33b0f
Ekaterina <katya_usova@bk.ru>
2023-02-11 22:50:44 +0300

    HomeWork64

---
+++ HomeWork64/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write("N = ");
+int N = System.Convert.ToInt32(System.Console.ReadLine());
+
+Natural(N);
+System.Console.ReadKey();
+            
+void Natural(int n)
+{
+    if (n != 0)
+    {
+        System.Console.Write("{0}, ", n);
+        n--;
+        Natural(n);
+    }
+}
